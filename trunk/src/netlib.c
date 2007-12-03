adc6b024c4f5340a0748064808a09ea71711578b
raj <raj@5bbd99f3-5903-0410-b283-f1d88047b228>
2007-12-03 20:07:56 +0000

    track further interface changes in sched.h

--- trunk/src/netlib.c
+++ trunk/src/netlib.c
@@ -1990,8 +1990,13 @@ bind_to_specific_processor(int processor_affinity, int use_cpu_map)
 
 #if defined(__CPU_SETSIZE)
 #define NETPERF_CPU_SETSIZE __CPU_SETSIZE
+#if defined(__CPU_SET_S)
+#define NETPERF_CPU_SET(cpu, cpusetp)  __CPU_SET_S(cpu, sizeof (cpu_set_t), cpusetp)
+#define NETPERF_CPU_ZERO(cpusetp)      __CPU_ZERO_S (sizeof (cpu_set_t), cpusetp) 
+#else
 #define NETPERF_CPU_SET(cpu, cpusetp)  __CPU_SET(cpu, cpusetp)
 #define NETPERF_CPU_ZERO(cpusetp)      __CPU_ZERO (cpusetp)
+#endif
   typedef cpu_set_t netperf_cpu_set_t;
 #else
 #define NETPERF_CPU_SETSIZE sizeof(unsigned long)
