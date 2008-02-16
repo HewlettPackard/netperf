927a353689209ae4d1d9f52ffed1ab4aee3b8a8a
raj <raj@5bbd99f3-5903-0410-b283-f1d88047b228>
2008-02-16 17:36:46 +0000

    nits

--- src/netrt_rtmget.c
+++ src/netrt_rtmget.c
@@ -1,5 +1,8 @@
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <net/if.h>
+#include <net/route.h>
 
 char     *
 find_egress_interface(struct sockaddr *source, struct sockaddr *dest) {
