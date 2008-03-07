61ce439cf8734c3604fff6b69bb9bb456f7238ff
raj <raj@5bbd99f3-5903-0410-b283-f1d88047b228>
2008-03-07 23:46:19 +0000

    ux fixes

--- trunk/src/netrt_none.c
+++ trunk/src/netrt_none.c
@@ -1,4 +1,14 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
 
 char *
 find_egress_interface(struct sockaddr *source, struct sockaddr *dest) {
