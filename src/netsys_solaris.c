27b63349636e28aad6640b572fc606853e8cdd31
raj <raj@5bbd99f3-5903-0410-b283-f1d88047b228>
2008-03-12 20:16:47 +0000

    solaris of all things becomes the first to be able to retrieve all three of system model cpu model and cpu frequency at least on x86

--- src/netsys_solaris.c
+++ src/netsys_solaris.c
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -66,12 +70,42 @@ find_cpu_model_freq(char **cpu_model, int *frequency) {
   kstat_close(kc);
 }
 
+static void
+find_system_model(char **system_model) {
+
+#if defined(HAVE_SYS_SMBIOS_H)
+#include <sys/smbios.h>
+  smbios_hdl_t *smbios_handle;
+  smbios_info_t info; 
+
+  int error;
+  int  ret;
+  id_t ret_id_t;
+
+  /* much of this is wild guessing based on web searches, sys/smbios.h, and
+     experimentation.  my thanks to a helpful person familiar with libsmbios
+     who got me started.  feel free to make yourself known as you see fit :)
+     rick jones 2008-03-12 */
+  smbios_handle = smbios_open(NULL,SMB_VERSION,0,&error);
+  ret = smbios_info_common(smbios_handle,256,&info);
+  if (0 == ret) 
+    *system_model = strdup(info.smbi_product);
+  else
+    *system_model = strdup("smbios_info_common");
+  smbios_close(smbios_handle);
+
+#else
+  *system_model = strdup("Teach me for SPARC");
+#endif
+
+  return;
+}
 
 void
 find_system_info(char **system_model, char **cpu_model, int *cpu_frequency) {
   int ret;
 
-  *system_model = strdup("Teach Me DMI");
+  find_system_model(system_model);
   find_cpu_model_freq(cpu_model,cpu_frequency);
 
 }
