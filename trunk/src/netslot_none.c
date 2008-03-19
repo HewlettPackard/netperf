6e73433d3a2fbc7ea201aecacbfaec08d2a0a80e
raj <raj@5bbd99f3-5903-0410-b283-f1d88047b228>
2008-03-19 22:59:12 +0000

    take a guess as to driver name on solaris by stripping cruft from the interface name

--- trunk/src/netslot_none.c
+++ trunk/src/netslot_none.c
@@ -1,3 +1,15 @@
+#if defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif
+
+#if defined(HAVE_STRING_H)
+#include <string.h>
+#endif
+
+#if defined(NETPERF_STANDALONE_DEBUG)
+#include <stdio.h>
+#endif
+
 char *
 find_interface_slot(char *interface_name) {
   return strdup("Not Implemented");
@@ -11,3 +23,34 @@ find_interface_ids(char *interface_name, int *vendor, int *device, int *sub_vend
   *sub_dev = 0;
   return;
 }
+
+#if defined(NETPERF_STANDALONE_DEBUG)
+int
+main(int argc, char *argv[]) {
+
+  char *slot;
+  int vendor;
+  int device;
+  int subvendor;
+  int subdevice;
+
+  if (argc != 2) {
+    fprintf(stderr,"%s <interface>\n",argv[0]);
+    return -1;
+  }
+
+  slot = find_interface_slot(argv[1]);
+
+  find_interface_ids(argv[1], &vendor, &device, &subvendor, &subdevice);
+
+  printf("%s in in slot %s: vendor %4x device %4x subvendor %4x subdevice %4x\n",
+	 argv[1],
+	 slot,
+	 vendor,
+	 device,
+	 subvendor,
+	 subdevice);
+
+  return 0;
+}
+#endif
