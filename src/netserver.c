HewlettPackard/netperf

commit 5380b1f5b67fa97f7574b73478a3cf4301f7707a

netserver: use mkstemp to create/open debug file
This is another attempt to address the primary issue:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=413658

similar to the original patch proposed:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?att=1;bug=413658;filename=CVE-2007-1444.patch;msg=35

src/netserver.c
@@ -254,20 +254,23 @@ open_debug_file()
   FILE *rd_null_fp;
 
   if (where != NULL) fflush(where);
-
-  snprintf(FileName,
-	   sizeof(FileName),
+  if (suppress_debug) {
+    FileName = NETPERF_NULL;
+    where = fopen(Filename, "w");
+  } else {
+    snprintf(FileName, sizeof(FileName),
 #if defined(WIN32)
-	   "%s\\%s_%d",
-	   getenv("TEMP"),
+	     "%s\\%s_XXXXXX", getenv("TEMP"),
 #else
-	   "%s_%d",
+	     "%s_XXXXXX",
 #endif
-	   DEBUG_LOG_FILE,
-	   getpid());
-  if ((where = fopen((suppress_debug) ? NETPERF_NULL : FileName,
-		     "w")) == NULL) {
-    perror("netserver: debug file");
+	     DEBUG_LOG_FILE);
+    where = mkstemp(FileName);
+  }
+
+  if (where == NULL) {
+    fprintf(stderr, "netserver: %s debug file : %s",
+	    FileName, strerror(errno));
     exit(1);
   }
 

commit 11dd5bb3308f4f352b30e0725faa3c18f89e6eb1

netserver: fix FileName usage
I introduce compile breakage with previous commit:
  https://github.com/HewlettPackard/netperf/commit/5380b1f5b67fa97f7574b73478a3cf4301f7707a

1) FileName is an array and not a pointer - treat it like one. :)
2) use correct variable name ("FileName" not "Filename")

src/netserver.c
@@ -256,8 +256,8 @@ open_debug_file()
 
   if (where != NULL) fflush(where);
   if (suppress_debug) {
-    FileName = NETPERF_NULL;
-    where = fopen(Filename, "w");
+    strncpy(FileName, NETPERF_NULL, sizeof(FileName));
+    where = fopen(FileName, "w");
   } else {
     snprintf(FileName, sizeof(FileName), "%s" FILE_SEP "%s",
              DEBUG_LOG_FILE_DIR, DEBUG_LOG_FILE);