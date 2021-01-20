2d88bcc75d97f462eafe8605f8da0c1f875b7dad
Gavin Brebner <gavin.brebner@hpe.com>
2021-01-20 16:04:54 +0100

    Added/updated HPE copyright messages

--- src/netcpu_ntperf.c
+++ src/netcpu_ntperf.c
@@ -1,5 +1,28 @@
+/*
+#  Copyright 2021 Hewlett Packard Enterprise Development LP 
+#  
+# Permission is hereby granted, free of charge, to any person obtaining a  
+# copy of this software and associated documentation files (the "Software"),  
+# to deal in the Software without restriction, including without limitation  
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+# and/or sell copies of the Software, and to permit persons to whom the  
+# Software is furnished to do so, subject to the following conditions: 
+#  
+# The above copyright notice and this permission notice shall be included in  
+# all copies or substantial portions of the Software. 
+#  
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+#  
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR  
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  
+# USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
 char   netcpu_ntperf_id[]="\
-@(#)netcpu_ntperf.c (c) Copyright 2005-2012, Hewlett-Packard Company, Version 2.6.0";
+@(#)netcpu_ntperf.c (c) Copyright 2005-2012, Hewlett-Packard Company, 2021 Hewlett Packard Enterprise Development LP, Version 2.6.0";
 
 #if HAVE_CONFIG_H
 # include <config.h>
