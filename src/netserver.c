HewlettPackard/netperf

commit 5380b1f5b67fa97f7574b73478a3cf4301f7707a

netserver: use mkstemp to create/open debug file
This is another attempt to address the primary issue:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=413658

similar to the original patch proposed:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?att=1;bug=413658;filename=CVE-2007-1444.patch;msg=35

src/netserver.c
@@ -254,20 +254,23 @@ open_debug_file()
   FILE *rd_null_fp;
 
   if (where != NULL) fflush(where);
-
-  snprintf(FileName,
-	   sizeof(FileName),
+  if (suppress_debug) {
+    FileName = NETPERF_NULL;
+    where = fopen(Filename, "w");
+  } else {
+    snprintf(FileName, sizeof(FileName),
 #if defined(WIN32)
-	   "%s\\%s_%d",
-	   getenv("TEMP"),
+	     "%s\\%s_XXXXXX", getenv("TEMP"),
 #else
-	   "%s_%d",
+	     "%s_XXXXXX",
 #endif
-	   DEBUG_LOG_FILE,
-	   getpid());
-  if ((where = fopen((suppress_debug) ? NETPERF_NULL : FileName,
-		     "w")) == NULL) {
-    perror("netserver: debug file");
+	     DEBUG_LOG_FILE);
+    where = mkstemp(FileName);
+  }
+
+  if (where == NULL) {
+    fprintf(stderr, "netserver: %s debug file : %s",
+	    FileName, strerror(errno));
     exit(1);
   }
 

commit fd9e2948486a452a9c936451f613b05d8ca8c19e

netserver: do not chmod("/dev/null", 0644) when suppress_debug==1 (default).
When running as root, chmod("/dev/null", 0644) will break most Unix-like
systems as "/dev/null" can't be open for write by non-root users anymore.

Before the fix:
root@linux# strace -f -e chmod ./netserver
Starting netserver with host 'IN(6)ADDR_ANY' port '12865' and family AF_UNSPEC
strace: Process 976220 attached
[pid 976219] +++ exited with 0 +++
chmod("/dev/null", 0644)                = 0

Fixes: 5380b1f5 ("netserver: use mkstemp to create/open debug file")

src/netserver.c
@@ -260,7 +260,10 @@ open_debug_file()
 
 #if !defined(WIN32)
 
-  chmod(FileName,0644);
+  /* Only chmod() if FileName != "/dev/null" */
+  if (strcmp(FileName, NETPERF_NULL)) {
+    chmod(FileName, 0644);
+  }
 
   /* redirect stdin to "/dev/null" */
   rd_null_fp = fopen(NETPERF_NULL,"r");